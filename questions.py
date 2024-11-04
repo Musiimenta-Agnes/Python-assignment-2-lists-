cd21aef8bcca0bedc51d8e1a03263245690367bf
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2024-11-04 13:22:45 +0300

    assignment

--- questions.py
+++ questions.py
@@ -1,35 +1,47 @@
 #From this list, ['Sandra', 'Patricia', 'Phionah', 'Anitah'].
 
-# a). Add the name Masha at the 4th position.
-studen_list = ['Sandra','Patrica', 'Phionah', 'Anitah']
-studen_list.insert(4, 'Masha')
-print(studen_list)
+
+# a).Print Patricia, Faith, Phionah and Anitah.
+student_list = ['Sandra', 'Patricia', 'Phionah', 'Anitah']
+student_marks = [80,56,78,90]
+
+student_list.insert(2, 'Faith')
+student_list.remove('Sandra')
+print(f"TThe updated list of with Faith added and Sandra removed is : {student_list}")
 
 
-# b). Update the name Phionah to Phionah Aladinah.
+
+# # a). Add the name Masha at the 4th position.
+student_list = ['Sandra','Patrica', 'Phionah', 'Anitah']
+student_list.insert(4, 'Masha')
+print(f" The updated list with Masha in the third position is : {student_list}")
+
+
+# # b). Update the name Phionah to Phionah Aladinah.
 
 new_name = 'Phionah Aladinah'
 index = 2
-studen_list[index] = new_name
-print(studen_list)
+student_list[index] = new_name
+print(f"The new updated list with the name Phionah replaced with Phionah Aladinah is : {student_list}")
 
 
-# c). Display the length of the students list. 
-length = len(studen_list)
-print(length)
+# # c). Display the length of the students list. 
+length = len(student_list)
+print(f" The length of the student list is : {length}")
 
 
-# d). Print all the student names using a for loop.
+# # d). Print all the student names using a for loop.
 
-for x in studen_list:
+for x in student_list:
     print(x)
 
 
 
-# 2. From the mark list [80,56,78,90].
-# b). Calculate the total marks for the student marks variable and the answer should be 304.
-marks = [80,56,78,90]
+# # 2. From the mark list [80,56,78,90].
+# # b). Calculate the total marks for the student marks variable and the answer should be 304.
+
 sum = 0
-for x in marks:
-    sum+=x
-    print(sum)
+student_marks = [80, 56, 78, 90]
+for mark in student_marks:
+        sum+=mark
+print(f"The total marks for all student marks is {sum}")
