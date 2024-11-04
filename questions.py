9841f0c66b26e545e355529a7963497780fc68bb
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2024-11-04 13:27:15 +0300

    assignment3

--- questions.py
+++ questions.py
@@ -1,4 +1,4 @@
-#From this list, ['Sandra', 'Patricia', 'Phionah', 'Anitah'].
+#From this list, ['Sandra', 'Patricia', 'Phionah', 'Anitah'] and marks marks = [80,56,78,90].
 
 
 # a).Print Patricia, Faith, Phionah and Anitah.
@@ -11,13 +11,13 @@ print(f"TThe updated list of with Faith added and Sandra removed is : {student_l
 
 
 
-# # a). Add the name Masha at the 4th position.
+ # b). Add the name Masha at the 4th position.
 student_list = ['Sandra','Patrica', 'Phionah', 'Anitah']
 student_list.insert(4, 'Masha')
 print(f" The updated list with Masha in the third position is : {student_list}")
 
 
-# # b). Update the name Phionah to Phionah Aladinah.
+# c). Update the name Phionah to Phionah Aladinah.
 
 new_name = 'Phionah Aladinah'
 index = 2
@@ -25,20 +25,19 @@ student_list[index] = new_name
 print(f"The new updated list with the name Phionah replaced with Phionah Aladinah is : {student_list}")
 
 
-# # c). Display the length of the students list. 
+# d). Display the length of the students list. 
 length = len(student_list)
 print(f" The length of the student list is : {length}")
 
 
-# # d). Print all the student names using a for loop.
+# #e). Print all the student names using a for loop.
 
 for x in student_list:
     print(x)
 
 
 
-# # 2. From the mark list [80,56,78,90].
-# # b). Calculate the total marks for the student marks variable and the answer should be 304.
+# f). Calculate the total marks for the student marks variable and the answer should be 304.
 
 sum = 0
 student_marks = [80, 56, 78, 90]
